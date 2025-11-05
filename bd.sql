a9b58f074f80926f142b4e556ed50685e32e5f62
Gabriel Ricca <gaboricca@gmail.com>
2025-11-05 16:14:19 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -8,3 +8,13 @@ create table usuarios (
     senha varchar (200) not null
     ) default charset = utf8mb4;
     
+CREATE TABLE funcionario (
+    id_func INT AUTO_INCREMENT PRIMARY KEY,
+    nome_func VARCHAR(150) NOT NULL,
+    email_func VARCHAR(150) NOT NULL UNIQUE,
+    data_nasc DATE NOT NULL,
+    cpf VARCHAR(15) NOT NULL UNIQUE,
+    senha_func VARCHAR(150) NOT NULL,
+    cargo_func ENUM('Vendedor', 'Gerente', 'Supervisor', 'Analista', 'Assistente', 'Coordenador', 'Diretor') NOT NULL,
+    salario_func DECIMAL(10,2) NOT NULL
+) DEFAULT CHARSET = utf8mb4;
